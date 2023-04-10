85ed056518a1b408a50967fd16582820ad66ac1a
Kasualix <670067575@qq.com>
2023-04-10 12:26:55 +0800

    port to 1.18

--- src/main/java/com/teampotato/enchantato/mixin/MixinEnchantment.java
+++ src/main/java/com/teampotato/enchantato/mixin/MixinEnchantment.java
@@ -1,8 +1,8 @@
 package com.teampotato.enchantato.mixin;
 
 import com.teampotato.enchantato.Enchantato;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.item.ItemStack;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraftforge.registries.ForgeRegistryEntry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
