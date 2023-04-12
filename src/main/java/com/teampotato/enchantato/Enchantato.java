bc9c4b085b9d4ef7cc32c8059aaf3ee9f055166f
Kasualix <670067575@qq.com>
2023-04-12 13:03:22 +0800

    fix

--- src/main/java/com/teampotato/enchantato/Enchantato.java
+++ src/main/java/com/teampotato/enchantato/Enchantato.java
@@ -2,7 +2,9 @@ package com.teampotato.enchantato;
 
 import com.google.common.collect.Lists;
 import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 
 import java.util.List;
 
@@ -10,6 +12,10 @@ import java.util.List;
 public class Enchantato {
     public static ForgeConfigSpec COMMON_CONFIG;
     public static ForgeConfigSpec.ConfigValue<List<? extends String>> ENCHANTMENT_LIST;
+
+    public Enchantato() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, COMMON_CONFIG);
+    }
 
     static {
         ForgeConfigSpec.Builder CONFIG_BUILDER = new ForgeConfigSpec.Builder();
